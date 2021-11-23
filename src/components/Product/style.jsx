a4937588444cd50966491c3372160ed839debf13
juyeong-chungbuk <happyu9986@gmail.com>
2021-11-23 23:54:03 +0900

    [ADD]상품 컴포넌트 스타일 추가

---
+++ src/components/Product/style.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Product = styled.div`
+  .container {
+    items-align: center;
+    display: flex;
+    height: 100%;
+    width: 100%;
+    max-width: 30rem;
+  }
+`
+
+export default Product;
