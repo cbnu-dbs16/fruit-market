826b62d45df6eb5151f8541ea24c595cfac50727
juyeong-chungbuk <happyu9986@gmail.com>
2021-11-29 02:02:44 +0900

    [EDIT]App스타일 추가

--- src/styles/Appstyle.js
+++ src/styles/Appstyle.js
@@ -2,6 +2,36 @@ import styled from 'styled-components'
 
 const App = styled.div`
     margin: auto;
+    .header{
+        .logo{
+            float :left;
+        }
+        .account{
+            float: right;
+        }   
+    }
+    .gnb {
+        background-color: white;
+        text-align: center;
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 28px 0px;
+        .list_nav{
+            display: block;
+            margin: 1em auto;
+            .nav_item {
+                color: black;
+                display: inline-flex;
+                margin: 10px 100px;
+                text-align: center;
+                .nav {
+                  font-size: 1rem;
+                }
+            }
+            .active{
+                color: #14148C;
+                font-weight: bold;
+            }
+        }      
+    }
 `
 
 export default App
