1e9400f4067f74d0f3690d9aac63a77094908b48
juyeong-chungbuk <happyu9986@gmail.com>
2021-11-30 20:06:06 +0900

    [EDIT]login component ui 수정

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,15 +1,66 @@
-import React from "react";
+import { React, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const Login = () => {
-    const user = {
-        'userid': 'id',
-        'userpwd': 'password'
-    };
-    return (
-        <div className="container">
-            {user.userid}
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    // formState: { errors },
+  } = useForm()
+
+//   const { isLoggedIn } = useSelector(state => state.auth)
+//   const dispatch = useDispatch()
+
+  const handleLogin = data => {
+    // console.log(data.userId, data.passWord)
+    // dispatch(login(data.userId, data.passWord))
+    //   .then(() => {
+    //     window.location.reload()
+    //   })
+    //   .catch(() => {
+    //   })
+  }
+
+  return (
+    <div className="container">
+      <div className="card card-container">
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-group">
+            <label htmlFor="userId">아이디</label>
+            <input
+              type="text"
+              className="form-control"
+              id="userId"
+              name="userId"
+              placeholder="아이디"
+              onChange={e => setValue('userId', e.target.value)}
+              //   onChange={onChangeUserid}
+              {...register('userId', { required: true })}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="passWord">비밀번호</label>
+            <input
+              type="password"
+              className="form-control"
+              id="passWord"
+              name="passWord"
+              placeholder="비밀번호"
+              //   onChange={onChangePassword}
+              {...register('passWord', { required: true })}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" value="Login" className="btn btn-primary btn-block">
+              <span>Login</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
