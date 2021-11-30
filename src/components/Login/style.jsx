66c46706cb62f98e929bdc8b35f18f8328e5aa29
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-01 02:55:33 +0900

    [EDIT]login 전체적인 스타일 구성 변경

---
+++ src/components/Login/style.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Login = styled.div`
+    .accountservice{
+        .service_register{
+            float: left;
+            &:hover{
+                font-weight: bold;
+            }
+        }
+        .login_search{
+            float: right;     
+        }
+    }
+`
+
+export default Login;
