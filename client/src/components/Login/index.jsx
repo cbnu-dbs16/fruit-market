131d6e07d3ec4953dd5b7ac2d13cd18e56576298
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-01 15:51:30 +0900

    login 재반영

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,15 +1,112 @@
-import React from "react";
+import * as React from 'react';
+import { useForm } from 'react-hook-form';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { makeStyles } from '@mui/styles';
+import StyledLogin from './style';
+
+const useStyles = makeStyles({
+    root: {
+        margin: '100px auto',
+        width: '100%',
+    },
+    h1: {
+        fontSize: '28px',
+    },
+    loginbtn: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      fontSize: '18px',
+      height: 55,
+      padding: '0 30px',
+    },
+  });
 
 const Login = () => {
-    const user = {
-        'userid': 'id',
-        'userpwd': 'password'
-    };
-    return (
-        <div className="container">
-            {user.userid}
-        </div>
-    );
+
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+        <h1 className={classes.h1}>
+            로그인
+        </h1>
+        <FormControl sx={{ m: '20px 0px', width: '100%' }} variant="standard">
+        <TextField
+          id="standard-textarea"
+          className={classes.input}
+          label="아이디"
+          multiline
+          variant="standard"
+        />
+        </FormControl><br/>
+        <FormControl sx={{ m: '15px 0px', width: '100%' }} variant="standard">
+            <InputLabel htmlFor="standard-adornment-password">비밀번호</InputLabel>
+            <Input
+              id="standard-adornment-password"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+        </FormControl>
+        <FormControl sx={{ m: '15px 0px', width: '100%' }} variant="standard">
+            <Button variant="contained" className={classes.loginbtn}>로그인</Button>
+        </FormControl>
+        <StyledLogin>
+            <div className="accountservice">
+                <a href="" className="service_register">회원가입</a>
+                <div className="login_search">
+                    <a href="" id="search_id">아이디 찾기</a> |
+                    <a href="" id="search_pwd"> 비밀번호 찾기</a>
+                </div>
+            </div>
+        </StyledLogin>
+    </div>
+  )
 }
 
-export default Login;
+export default Login
--- client/src/styles/Appstyle.js
+++ client/src/styles/Appstyle.js
@@ -1,37 +1,48 @@
 import styled from 'styled-components'
 
 const App = styled.div`
-    margin: auto;
-    .header{
-        .logo{
-            float :left;
+    .App{
+        // position: relative;
+        min-width: 1050px;
+        max-width: 1500px;
+        background-color: #fff;
+        margin: 0px auto;
+        .header{
+            .logo{
+                float :left;
+            }
+            .account{
+                float: right;
+            }
         }
-        .account{
-            float: right;
-        }   
-    }
-    .gnb {
-        background-color: white;
-        text-align: center;
-        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 28px 0px;
-        .list_nav{
-            display: block;
-            margin: 1em auto;
-            .nav_item {
-                color: black;
-                display: inline-flex;
-                margin: 10px 100px;
-                text-align: center;
-                .nav {
-                  font-size: 1rem;
+        .gnb {
+            background-color: white;
+            text-align: center;
+            box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 28px 0px;
+            .list_nav{
+                display: block;
+                margin: 1em auto;
+                .nav_item {
+                    color: black;
+                    display: inline-flex;
+                    margin: 10px 80px;
+                    text-align: center;
+                    .nav {
+                      font-size: 1rem;
+                    }
                 }
-            }
-            .active{
-                color: #14148C;
-                font-weight: bold;
-            }
-        }      
-    }
+                .active{
+                    color: #14148C;
+                    font-weight: bold;
+                }
+            }      
+        }
+        .container{
+            margin: 0px auto;
+            max-width: 30rem;
+            text-align: center;
+        }
+    } 
 `
 
 export default App
