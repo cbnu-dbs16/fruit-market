edf0fb7a03ed475fb2fd5225c2ca946720d43757
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-03 03:19:42 +0900

    [EDIT]product 보여줄 아이템 컴포넌트 분리 및 스타일 수정

--- client/src/components/Product/style.jsx
+++ client/src/components/Product/style.jsx
@@ -2,8 +2,55 @@ import styled from 'styled-components'
 
 const Product = styled.div`
   .content{
-    
-  }  
+    .carticon{
+      position: relative;
+      bottom: -102.5%;
+      right: 3%;
+      background: #fff;
+      border-radius: 50px;
+      width: 14%;
+      padding: 6px 0;
+      float: right;
+      z-index: 8;
+      &:hover{
+        background: #3CA0FF	;
+      }
+    }
+    .quantity{
+      display: flex;
+      position: relative;
+      bottom: -110%;
+      left: 38%;
+      background: #fff;
+      opacity: 0.9;
+      border: 1px solid grey;
+      border-radius: 5px;
+      width: 88px;
+      height: 30px;
+      z-index: 8;
+      text-align: center;
+      font-size: 20px;
+    }
+  }
+`
+
+export const Optionstyle = styled.div`
+  .itemname{
+    // float : left;
+    // margin: 15px 0px;
+  }
+  .itemprice{
+    // float : left;
+    // margin: 15px 0px;
+  }
+`
+
+export const Option = styled.span`
+  .count{
+    border: 1px solid grey;
+    border-radius: 3px;
+    font-size: 14px;
+  }
 `
 
 export default Product;
