a5bfdb5ffc1c0aeb3cd5e5c6c4d980846bee3261
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-04 02:40:16 +0900

    [ADD]user 관련 action 함수 정의

---
+++ client/src/action/users.js
@@ -0,0 +1,22 @@
+import UserService from '../services/userservice';
+
+export const RegisterUser = async user => {
+    try {
+      const res = await UserService.register(user);
+      console.log(res);
+      return Promise.resolve(res.data.data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+}
+
+export const LoginUser = async user => {
+    try {
+      console.log(user);
+      const res = await UserService.login(user);
+      console.log(res);
+      return Promise.resolve(res.data.data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
