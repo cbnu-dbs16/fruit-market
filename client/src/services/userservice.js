3e3ea0bb32f52c401371c235454f3c93b34810e6
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-04 02:43:57 +0900

    [ADD]user register, login, idcheck 라우터로 연결

---
+++ client/src/services/userservice.js
@@ -0,0 +1,13 @@
+import http from '../common/http'
+
+const register = user => http.post(`Customer/`, user);
+const login = user => http.post(`Customer/`, user);
+const idcheck = userId => http.get(`Customer/check/${userId}`)
+
+const UserService = {
+  register,
+  login,
+  idcheck,
+}
+
+export default UserService
