53122e0f1af972a927ddb8dc5c97f8c6e110e2e1
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-05 01:57:28 +0900

    [ADD]product component 액션 생성 - 전체 과일 리스트 불러오기

---
+++ client/src/action/fruitlist.js
@@ -0,0 +1,13 @@
+import FruitService from '../services/fruitservice';
+
+export const GetList = async () => {
+    try {
+      const res = await FruitService.getlist();
+      console.log(res);
+      return Promise.resolve(res.data.data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+}
+export default GetList;
+
