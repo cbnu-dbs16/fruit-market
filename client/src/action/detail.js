583ba478145fa0c793b0b3c7117563b18381102c
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-05 01:57:03 +0900

    [ADD]detail component 액션 생성 - 클릭한 아이템 정보 불러오기

---
+++ client/src/action/detail.js
@@ -0,0 +1,13 @@
+import FruitService from '../services/fruitservice';
+
+export const GetDetailItem = async itemname => {
+    try {
+      const res = await FruitService.getdetail(itemname);
+      console.log(res);
+      return Promise.resolve(res.data.data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+}
+export default GetDetailItem;
+
