42680fcce29be2c1efef4f6a49e4271fd2eb8ca7
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-05 02:00:59 +0900

    [ADD]cart component -틀 구성

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Bringcartlist } from "../../action/carts";
+import StyledCart from './style';
+import EachItem from './eachitem';
 
 const Cart = () => {
+    const [itemincart, setitemincart] = useState([{
+        img: '',
+        itemname: '',
+        price: 0,
+        count: 0,
+    }]);
+
+    useEffect(()=>{
+        Bringcartlist()
+        .then(e => {
+            console.log(e);
+            setitemincart({...itemincart, 
+                img: '',
+                itemname: '',
+                price: 0,
+                count: 0, });
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    })
+
     return (
-        <div className="container">
-            Cart
-        </div>
+        <StyledCart>
+            <div className="content">
+                Cart
+                <div className="listincart">
+                    {itemincart.map((item)=>{
+                        <EachItem key={item.itemname} item={item}/>
+                    })}
+                </div>
+            </div>
+        </StyledCart>
     )
 }
 export default Cart;
