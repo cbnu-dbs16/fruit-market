a16a9005b0e0e22c703475f2d3ba432a7273b04e
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-05 02:00:11 +0900

    [EDIT]product/option-서버쪽에서 데이터 받아오도록 변경

--- client/src/components/Product/option.jsx
+++ client/src/components/Product/option.jsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
 import Option from './style';
-// import Optionstyle from './style';
 import ImageListItem from '@mui/material/ImageListItem';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { AddFriut } from '../../action/carts';
+import { Link } from "react-router-dom"
 
 function Optionitem(props){
-    const [item, setItemData] = useState({...props.item});
+    const [item, setItem] = useState({...props.item});
 
     const decreasecnt = () => {
         if(item.count > 1)
-            setItemData({...item, count: item.count - 1, price: (item.price / item.count) * (item.count - 1)});
+            setItem({...item, count: item.count - 1, price: (item.price / item.count) * (item.count - 1)});
     }
     const increasecnt = () => {
         if(item.count >= 1)
-            setItemData({...item, count: item.count + 1, price: (item.price / item.count) * (item.count + 1)});
+            setItem({...item, count: item.count + 1, price: (item.price / item.count) * (item.count + 1)});
     }
     const handleAddcart = () => {
         // const item = {
@@ -34,7 +34,7 @@ function Optionitem(props){
             <Option>
                 <span className="count">
                 <button type="button" onClick={()=>{decreasecnt()}}>-</button>
-                <input type="number" readOnly="readonly" value={item.count} style={{width: '30px', height: '30px', border: 0, padding: '0px 0px 0px 10px', marginRight: '-1px', lineHeight: '18px', textAlign: 'center'}}/>
+                <input type="number" readOnly="readonly" value={item.count} style={{width: '30px', height: '30px', border: 0, padding: '0px 0px 0px 15px', textAlign: 'center'}}/>
                 <button type="button" onClick={()=>{increasecnt()}}>+</button>
                 </span>
             </Option>
@@ -42,20 +42,17 @@ function Optionitem(props){
             <div className="carticon" onClick={handleAddcart}>
             <AddShoppingCartIcon />
             </div>
-            <ImageListItem key={item.img} style={{ margin: '120px 5px'}}>
-            <img
-                src={`${item.img}`}
-                alt={item.fruitname}
-                />
-            {/* <Optionstyle> */}
-                <div className="itemname" style={{ float : 'left', margin: '15px 0px'}}>
-                    <p>{item.fruitname}</p>
-                </div>
-                <div className="itemprice" style={{ float : 'left', margin: '15px 0px'}}>
-                <p>{item.price}$</p>
-                </div>
-            {/* </Optionstyle> */}
-            </ImageListItem>
+            <Link to={`/detail/${item.fruitname}`}>
+                <ImageListItem key={item.img} style={{ margin: '120px 5px'}}>
+                    <img src={`${item.img}`} alt={item.fruitname}/> 
+                    <div className="itemname" style={{ float : 'left', margin: '15px 0px'}}>
+                        <p>{item.fruitname}</p>
+                    </div>
+                    <div className="itemprice" style={{ float : 'left', margin: '15px 0px'}}>
+                    <p>{item.price}$</p>
+                    </div>
+                </ImageListItem>
+            </Link>
         </div>
     );
 }
