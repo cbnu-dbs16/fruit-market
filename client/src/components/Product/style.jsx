b842dfd3d0bbbb95eb2cea81761c622a94f81da8
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-05 01:59:37 +0900

    [ADD]product component style- detail style 추가

--- client/src/components/Product/style.jsx
+++ client/src/components/Product/style.jsx
@@ -2,18 +2,20 @@ import styled from 'styled-components'
 
 const Product = styled.div`
   .content{
+    margin: 0px auto;
     .carticon{
       position: relative;
-      bottom: -102.5%;
+      bottom: -101.5%;
       right: 3%;
       background: #fff;
       border-radius: 50px;
       width: 14%;
-      padding: 6px 0;
+      padding: 8px 0;
       float: right;
       z-index: 8;
       &:hover{
-        background: #3CA0FF	;
+        background: linear-gradient(50deg, #A814E7 30%, #288CD2 80%);
+        color: white;
       }
     }
     .quantity{
@@ -28,28 +30,64 @@ const Product = styled.div`
       width: 88px;
       height: 30px;
       z-index: 8;
-      text-align: center;
-      font-size: 20px;
     }
   }
 `
 
-export const Optionstyle = styled.div`
-  .itemname{
-    // float : left;
-    // margin: 15px 0px;
+export const StyledDetail = styled.div`
+  .content{
+    display: flex;
+    width: 1050px;
+    margin: 0 auto;
+    padding-top: 20px;
   }
-  .itemprice{
-    // float : left;
-    // margin: 15px 0px;
+  .items_info{
+    float: right;
+    width: 560px;
+    margin: 0 auto;
+    padding-top: 20px;
+    border-top: 1px solid #f4f4f4;
   }
-`
+  .items_info .tit{
+    float: left;
+    width: 150px;
+    font-size: 16px;
+    line-height: 20px;
+  }
+  .items_info .list.fst {
+    padding-bottom: 0;
+    border-bottom: 0;
+  }
+  .items_info .list {
+    padding: 20px 0;
+    border-top: 1px solid #f4f4f4;
+  }
+  .detail_description{
 
-export const Option = styled.span`
-  .count{
-    border: 1px solid grey;
-    border-radius: 3px;
-    font-size: 14px;
+  }
+  p{
+    .golist{
+      font-size: 20px;
+    }
+    .items_name {
+      float: right;
+      width: 560px;
+      padding: 10px 0 29px;
+      margin: 20px auto;
+    }
+  }
+  span{
+    .count{
+      border: 1px solid grey;
+      border-radius: 3px;
+      font-size: 14px;
+      width: 150px;
+      height: 150px;
+    }
+  }
+  dl{
+    display: block;
+    margin: 1.2em 0 1.2em 0;
   }
 `
 
