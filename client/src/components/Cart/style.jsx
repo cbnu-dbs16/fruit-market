adb71f9963a90e680baef10db42df053b03e0db5
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-05 02:00:37 +0900

    [ADD]cart component style 추가

---
+++ client/src/components/Cart/style.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledCart = styled.div`
+    .content{
+        // display: flex;
+        width: 1050px;
+        margin: 0 auto;
+        padding-top: 20px;
+    }
+`
+
+export default StyledCart;
