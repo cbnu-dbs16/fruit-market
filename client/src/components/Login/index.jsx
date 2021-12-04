433f538f2c817d3e030365e211ebae7c20b640d1
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-05 02:00:20 +0900

    [EDIT]버튼 색상 변경

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -22,7 +22,7 @@ const useStyles = makeStyles({
         fontSize: '28px',
     },
     loginbtn: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      background: 'linear-gradient(45deg, #A814E7 30%, #288CD2 92%)',
       border: 0,
       borderRadius: 3,
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
