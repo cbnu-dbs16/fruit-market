2753f2df63593a978d45cb21bfdd5f9e7573f76c
cksl0830 <dkssudgkdl9@naver.com>
2021-12-04 19:20:23 +0900

    update server.js
    add

--- server/server.js
+++ server/server.js
@@ -11,20 +11,12 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
-const Storage = multer.diskStorage({
-    destination(req, file, callback){
-        callback(null, './uploads')
-    },
-    filename(req, file, callback){
-        callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
-    },
-})
 
 app.post('/api/Customer', (req, res) => {
     var body = req.body;
     console.log(body);
 
-    var sql = 'INSERT INTO Customer VALUES(?, ?, ?, ?)';
+    var sql = 'INSERT INTO Customer VALUES(?, ?, ?, ?, ?, ?)';
     var params = [body.id, body.password, body.username];
     console.log(sql);
     conn.query(sql, params, function(err){
@@ -32,6 +24,51 @@ app.post('/api/Customer', (req, res) => {
         else res.redirect('/api/Customer');
     })
 });
+
+app.get('/api/Customer', (req, res) => {
+    //console.log(body);
+    var sql = 'SELECT * FROM Customer WHERE cus_id = ? and cus_password = ?';
+    var params = [req.query.id, req.query.password];
+    console.log(sql);
+    
+    conn.query(sql,params, function (err, rows, fields){
+        if(err) console.log('Login failed..' + err);
+        else{
+            console.log('sql 결과 : '+JSON.stringify(rows))
+            res.send(rows);
+        }
+    })
+})
+
+app.post('/api/comments',(req, res) => {
+    var body = req.body;
+    console.log(body);
+
+    var sql = 'INSERT INTO comments VALUES(?, ?, ?, ?)';
+    var params = [body.cus_id, 1, body.comment, body.fno];
+    console.log(sql);
+    conn.query(sql, params, function(err){
+        if(err) console.log('Insertion failed.. ' + err);
+        
+        //else res.send(rows);
+    })
+});
+
+app.get('/api/comments', (req,res) => {
+    var body = req.query;
+    var sql = 'SELECT * FROM comments WHERE fno=?';
+    var params = [parseInt(body.fno)];
+    console.log(sql);
+    console.log(req.query);
+    conn.query(sql,params, function (err, rows, fields){
+        if(err) console.log('Load specify comments failed..' + err);
+        else{
+            console.log('sql 결과 : '+JSON.stringify(rows))
+            if(rows) res.send(rows);
+        }
+    })
+})
+
 
 app.get('/api/fruits', (req,res) => {
     var sql = 'SELECT * FROM fruits';
@@ -111,21 +148,6 @@ app.put('/api/fruits', (req, res) => {
         else{
             console.log('sql 결과 : '+JSON.stringify(rows))
             if(rows) res.send(rows);
-        }
-    })
-})
-
-app.get('/api/Customer', (req, res) => {
-    //console.log(body);
-    var sql = 'SELECT * FROM Customer WHERE cus_id = ? and cus_password = ?';
-    var params = [req.query.id, req.query.password];
-    console.log(sql);
-    
-    conn.query(sql,params, function (err, rows, fields){
-        if(err) console.log('Login failed..' + err);
-        else{
-            console.log('sql 결과 : '+JSON.stringify(rows))
-            res.send(rows);
         }
     })
 })
