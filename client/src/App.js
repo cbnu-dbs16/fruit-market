571a607797d21c7a188c57820861b01ade167c83
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-05 01:55:44 +0900

    [ADD]detail component 라우터 설정

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Product from './components/Product';
+import Detail from './components/Product/detail';
 import Cart from './components/Cart';
 import Mypage from './components/Mypage';
 import StyledApp from './styles/Appstyle';
@@ -31,16 +32,17 @@ function App() {
         </div>
         <div className="gnb">
           <ul className="list_nav">
-            <li className="nav_item"><Link to="" className="nav-link" onClick={() => setActiveNav(1)}><p className={activeNav === 1 ? "nav_item active" : "nav_item"}>홈</p></Link></li>
-            <li className="nav_item"><Link to="product" className="nav-link" onClick={() => setActiveNav(2)}><p className={activeNav === 2 ? "nav_item active" : "nav_item"}>과일 담기</p></Link></li>
-            <li className="nav_item"><Link to="cart" className="nav-link" onClick={() => setActiveNav(3)}><p className={activeNav === 3 ? "nav_item active" : "nav_item"}><ShoppingCartIcon /></p></Link></li>
-            <li className="nav_item"><Link to="mypage" className="nav-link" onClick={() => setActiveNav(4)}><p className={activeNav === 4 ? "nav_item active" : "nav_item"}><PersonIcon /></p></Link></li>
+          <Link to="" className="nav-link" onClick={() => setActiveNav(1)}><li className="nav_item"><p className={activeNav === 1 ? "nav_item active" : "nav_item"}>홈</p></li></Link>
+          <Link to="product" className="nav-link" onClick={() => setActiveNav(2)}><li className="nav_item"><p className={activeNav === 2 ? "nav_item active" : "nav_item"}>과일 담기</p></li></Link>
+          <Link to="cart" className="nav-link" onClick={() => setActiveNav(3)}><li className="nav_item"><p className={activeNav === 3 ? "nav_item active" : "nav_item"}><ShoppingCartIcon /></p></li></Link>
+          <Link to="mypage" className="nav-link" onClick={() => setActiveNav(4)}><li className="nav_item"><p className={activeNav === 4 ? "nav_item active" : "nav_item"}><PersonIcon /></p></li></Link>
           </ul>
         </div>
         <div className="container">
           <Routes>
             <Route path="" element={<Home />} />
             <Route path="product" exact element={<Product />} />
+            <Route path="detail/:name" exact element={<Detail />} />
             <Route path="cart" exact element={<Cart />} />
             <Route path="mypage" exact element={<Mypage />} />
             <Route path="register" element={<Register />} />
