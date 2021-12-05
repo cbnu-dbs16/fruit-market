d0727fd5316aeb90e0fe2749719406f0661fd2b9
cksl0830 <dkssudgkdl9@naver.com>
2021-12-05 17:21:43 +0900

    add detail
    수정

--- server/server.js
+++ server/server.js
@@ -16,11 +16,7 @@ app.post('/api/Customer', (req, res) => {
     var body = req.body;
     console.log(body);
 
-<<<<<<< HEAD
     var sql = 'INSERT INTO Customer VALUES(?, ?, ?, ?, ?, ?)';
-=======
-    var sql = 'INSERT INTO Customer VALUES(?, ?, ?, ?)';
->>>>>>> 449b3b8c3cf74a4be3db0fac8be439702f4977be
     var params = [body.id, body.password, body.username];
     console.log(sql);
     conn.query(sql, params, function(err){
@@ -29,7 +25,6 @@ app.post('/api/Customer', (req, res) => {
     })
 });
 
-<<<<<<< HEAD
 app.get('/api/Customer', (req, res) => {
     //console.log(body);
     var sql = 'SELECT * FROM Customer WHERE cus_id = ? and cus_password = ?';
@@ -75,8 +70,6 @@ app.get('/api/comments', (req,res) => {
 })
 
 
-=======
->>>>>>> 449b3b8c3cf74a4be3db0fac8be439702f4977be
 app.get('/api/fruits', (req,res) => {
     var sql = 'SELECT * FROM fruits';
     
@@ -91,7 +84,6 @@ app.get('/api/fruits', (req,res) => {
     })
 })
 
-<<<<<<< HEAD
 app.get('/api/fruits/detail', (req,res) => {
     var sql = 'SELECT * FROM fruits WHERE fno = ?';
     var params = [req.query.fno];
@@ -106,8 +98,6 @@ app.get('/api/fruits/detail', (req,res) => {
     })
 })
 
-=======
->>>>>>> 449b3b8c3cf74a4be3db0fac8be439702f4977be
 app.delete('/api/cart', (req,res) => {
     var body = req.body;
     var sql = 'DELETE FROM cart WHERE id=? and Fno=?';
@@ -118,23 +108,12 @@ app.delete('/api/cart', (req,res) => {
     })
 })
 
-<<<<<<< HEAD
 app.get('/api/cartList', (req,res) => {
     var sql = 'SELECT * FROM cart WHERE cus_id = ? and fno= ?';
     console.log(sql);
     var params = [req.query.id];
     conn.query(sql,params,  function (err, rows, fields){
         if(err) console.log('Load cart failed..' + err);
-=======
-app.get('/api/fruits/like', (req, res) => {
-    //console.log(body);
-    var sql = 'SELECT * FROM cart WHERE cus_id = ? and fno = ?';
-    var params = [req.query.cusid, parseInt(req.query.fno)];
-    console.log(sql);
-    console.log(req.query);
-    conn.query(sql,params, function (err, rows, fields){
-        if(err) console.log('Load cart like failed..' + err);
->>>>>>> 449b3b8c3cf74a4be3db0fac8be439702f4977be
         else{
             console.log('sql 결과 : '+JSON.stringify(rows))
             if(rows) res.send(rows);
@@ -142,79 +121,6 @@ app.get('/api/fruits/like', (req, res) => {
     })
 })
 
-<<<<<<< HEAD
-app.post('/api/cart', (req, res) => {
-    //console.log(body);
-    var body = req.body;
-    var sql = 'INSERT INTO cart VALUES (?, ?)';
-    var params = [body.cus_id, body.fno];
-=======
-app.get('/api/cartfruits', (req,res) => {
-    var sql = 'SELECT * FROM fruits WHERE fno = ?';
-    var params = [req.query.fno];
-    console.log(sql);
-    console.log(req.query);
-    conn.query(sql,params, function (err, rows, fields){
-        if(err) console.log('Load cart fruits failed..' + err);
-        else{
-            console.log('sql 결과 : '+JSON.stringify(rows))
-            if(rows) res.send(rows);
-        }
-    })
-})
-
-app.get('/api/cartList', (req,res) => {
-    var sql = 'SELECT * FROM cart WHERE cus_id = ?';
-    console.log(sql);
-    var params = [req.query.id];
-    conn.query(sql,params,  function (err, rows, fields){
-        if(err) console.log('Load cart failed..' + err);
-        else{
-            console.log('sql 결과 : '+JSON.stringify(rows))
-            if(rows) res.send(rows);
-        }
-    })
-})
-
-app.put('/api/fruits', (req, res) => {
-    //console.log(body);
-    var sql = 'UPDATE fruits SET state = ? WHERE fno = ?';
-    var params = [false, req.query.fno];
-    console.log(sql);
-    console.log(params);
-    console.log(req.query);
-    conn.query(sql,params, function (err, rows, fields){
-        if(err) console.log('Fruits state update failed..' + err);
-        else{
-            console.log('sql 결과 : '+JSON.stringify(rows))
-            if(rows) res.send(rows);
-        }
-    })
-})
-
-app.get('/api/Customer', (req, res) => {
-    //console.log(body);
-    var sql = 'SELECT * FROM Customer WHERE cus_id = ? and cus_password = ?';
-    var params = [req.query.id, req.query.password];
->>>>>>> 449b3b8c3cf74a4be3db0fac8be439702f4977be
-    console.log(sql);
-    console.log(params);
-    console.log(req.query);
-    conn.query(sql,params, function (err, rows, fields){
-<<<<<<< HEAD
-        if(err) console.log('Insert cart failed..' + err);
-=======
-        if(err) console.log('Login failed..' + err);
->>>>>>> 449b3b8c3cf74a4be3db0fac8be439702f4977be
-        else{
-            console.log('sql 결과 : '+JSON.stringify(rows))
-            if(rows) res.send(rows);
-        }
-    })
-})
-
-<<<<<<< HEAD
-=======
 app.post('/api/cart', (req, res) => {
     //console.log(body);
     var body = req.body;
@@ -232,5 +138,4 @@ app.post('/api/cart', (req, res) => {
     })
 })
 
->>>>>>> 449b3b8c3cf74a4be3db0fac8be439702f4977be
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
