708b446b544494157ceea0c319028c73eba40b0a
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-06 15:46:16 +0900

    [EDIT]cart component - 각 아이템 스타일수정

--- client/src/components/Cart/style.jsx
+++ client/src/components/Cart/style.jsx
@@ -9,4 +9,40 @@ const StyledCart = styled.div`
     }
 `
 
+export const StyledEachItem = styled.div`
+    ul{
+
+    }
+    li{
+        display: flex;
+        border-bottom: 1px solid #f4f4f4;
+        padding: 10px 0;
+        margin-top: -12px;
+    }
+    .item{
+        display: flex;
+        min-height: 128px;
+        padding: 0;
+        position: relative;
+    }
+    .inner_name{
+        width: 5em;
+        margin: auto 3em;
+    }
+    .price{
+        width: 23em;
+        margin: auto 3em;
+    }
+    label{
+        margin: auto 3em;
+    }
+    span{
+        margin: auto 3em;
+        border: 1px solid #e1e1e1;
+        width: 88px;
+        height: 30px;
+        border-radius: 3px
+    }
+`
+
 export default StyledCart;
