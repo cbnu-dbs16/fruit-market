ff8d373905c4cb6025643d33019c32cd543de2fd
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-08 01:27:08 +0900

    [EDIT]cart style수정

--- client/src/components/Cart/style.jsx
+++ client/src/components/Cart/style.jsx
@@ -2,10 +2,47 @@ import styled from 'styled-components'
 
 const StyledCart = styled.div`
     .content{
-        // display: flex;
         width: 1050px;
         margin: 0 auto;
         padding-top: 20px;
+        .cart_select {
+            position: static;
+            height: auto;
+            padding: 0 0 30px 0;
+            border-bottom: 1px solid #ddd;
+        }
+        .inner_select {
+            position: static;
+            padding: 0 10px 0 2px;
+            // border-bottom: 1px solid black;
+            margin: 0 30px;
+        }
+    }
+    h2{
+        font-weight: 600;
+        font-size: 28px;
+        color: #333;
+        line-height: 35px;
+        text-align: center;
+        letter-spacing: 1px;
+        padding: 2rem 0;
+    }
+    label{
+        font-weight: 700;
+        font-size: 14px;
+        margin: 0 10px;
+    }
+    a{
+        font-weight: 700;
+        font-size: 14px;
+        margin: 0 10px;
+    }
+    span{
+        margin: 0 5px;
+    }
+    hr{
+        // margin: 1.5em 0;
+        // color: grey;
     }
 `
 
@@ -43,6 +80,7 @@ export const StyledEachItem = styled.div`
         height: 30px;
         border-radius: 3px
     }
+    
 `
 
 export default StyledCart;
