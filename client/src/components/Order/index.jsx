779e7ed56fa7bdabe32f6501e469c4beba23265b
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-08 01:29:19 +0900

    [ADD]주문목록 컴포넌트 추가

---
+++ client/src/components/Order/index.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+const Order = () => {
+
+    useEffect(()=>{
+        // Bringcartlist()
+        // .then(e => {
+        //     console.log(e);
+        //     setitemincart({...itemincart, 
+        //         img: '',
+        //         itemname: '',
+        //         price: 0,
+        //         count: 0, });
+        // })
+        // .catch(e => {
+        //     console.log(e);
+        // })
+    })
+
+    return (
+        <div className="content">
+            <h2 className="tit">주문하기</h2>
+        </div>
+    )
+}
+export default Order;
