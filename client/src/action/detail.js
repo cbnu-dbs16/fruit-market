6db7545440ba9b323f8ebe85b0389abcc843b465
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-14 04:24:09 +0900

    edit: getdetail 인자명 변경

--- client/src/action/detail.js
+++ client/src/action/detail.js
@@ -1,8 +1,8 @@
 import FruitService from '../services/fruitservice';
 
-export const GetDetailItem = async itemname => {
+export const GetDetailItem = async fno => {
     try {
-      const res = await FruitService.getdetail(itemname);
+      const res = await FruitService.getdetail(fno);
       console.log(res);
       return Promise.resolve(res.data);
     } catch (err) {
