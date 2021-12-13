e10923f85483c1a597dd0b733b0afb35ad87e9d8
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-13 22:49:48 +0900

    edit: login post->get으로 수정

--- client/src/services/userservice.js
+++ client/src/services/userservice.js
@@ -1,7 +1,7 @@
 import http from '../common/http'
 
 const register = user => http.post(`Customer/`, user);
-const login = user => http.post(`Customer/`, user);
+const login = user => http.get(`Customer/`, user);
 const idcheck = userId => http.get(`Customer/check/${userId}`)
 
 const UserService = {
