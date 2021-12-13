9c90d47e69a221e324e22f40009ae3552e730f57
juyeong-chungbuk <happyu9986@gmail.com>
2021-12-14 04:24:31 +0900

    edit: item key명 변경

--- client/src/components/Cart/eachitem.jsx
+++ client/src/components/Cart/eachitem.jsx
@@ -8,14 +8,14 @@ import { DeleteFruit } from '../../action/carts';
 function EachItem(props){
     const [item, setItem] = useState({...props.item});
     const [select, setselect] = useState(props.select);
-    // console.log(item);
+    console.log(item);
     const decreasecnt = () => {
-        if(item.count > 1)
-            setItem({...item, count: item.count - 1, price: (item.price / item.count) * (item.count - 1)});
+        if(item.Count > 1)
+            setItem({...item, Count: item.Count - 1, price: (item.price / item.Count) * (item.Count - 1)});
     }
     const increasecnt = () => {
-        if(item.count >= 1)
-            setItem({...item, count: item.count + 1, price: (item.price / item.count) * (item.count + 1)});
+        if(item.Count >= 1)
+            setItem({...item, Count: item.Count + 1, price: (item.price / item.Count) * (item.Count + 1)});
     }
     const selectitem = () => {
         setselect(!select);
@@ -38,17 +38,17 @@ function EachItem(props){
                         <label className="check">
                             {select ? <CheckCircleIcon onClick={selectitem}/> : <CheckCircleOutlineIcon onClick={selectitem}/>}
                         </label>
-                        <img src={item.img} width={120} height={120}/>
+                        <img src={item.fimage} width={120} height={120}/>
                         <div className="inner_name">
-                            <p>{item.fruitname}</p>
+                            <p>{item.fname}</p>
                         </div>
                         <div className="price">
                             <span className="count">
                                 <button type="button" onClick={()=>{decreasecnt()}}>-</button>
-                                <input type="number" readOnly="readonly" value={item.count} style={{width: '30px', height: '30px', border: 0, padding: '0 0 0 1em', textAlign: 'center'}}/>
+                                <input type="number" readOnly="readonly" value={item.Count} style={{width: '30px', height: '30px', border: 0, padding: '0 0 0 1em', textAlign: 'center'}}/>
                                 <button type="button" onClick={()=>{increasecnt()}}>+</button>
                             </span>
-                            <span className="selling" style={{border: '0'}}>{item.price}원</span>
+                            <span className="selling" style={{border: '0'}}>{item.Price}원</span>
                         </div>     
                     </div>
                     <button type="button" className="btn_delete"><DeleteIcon onClick={deleteitem()}/></button>
